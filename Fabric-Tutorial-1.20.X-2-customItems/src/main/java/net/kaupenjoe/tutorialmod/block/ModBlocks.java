c561f117e7aff97023667e6d5c5b1700b049807b
CinammonToast <professotr56@gmail.com>
2024-03-22 13:21:45 +0300

    Initial commit

---
+++ Fabric-Tutorial-1.20.X-2-customItems/src/main/java/net/kaupenjoe/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,28 @@
+package net.kaupenjoe.tutorialmod.block;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.kaupenjoe.tutorialmod.TutorialMod;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK,  new Identifier(TutorialMod.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block) {
+        return Registry.register(Registries.ITEM, new Identifier(TutorialMod.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+    }
+
+
+    public static void registerModBlocks() {
+        TutorialMod.LOGGER.info("Registering ModBlocks for " + TutorialMod.MOD_ID);
+    }
+}
